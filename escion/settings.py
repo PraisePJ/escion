ca9230a089e5fee2fd7ac27413dd16f47aa02748
PraisePJ <93436815+PraisePJ@users.noreply.github.com>
2023-01-26 18:09:36 +0100

    Add files via upload

--- escion/settings.py
+++ escion/settings.py
@@ -27,7 +27,7 @@ with open('escion/key.txt', 'r') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['escion.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['sayon.up.railway.app', '127.0.0.1']
 
 
 # Application definition
